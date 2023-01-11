5875dc43cd4f46526cad9b2a8e49160b8b032c56
Dmitriy-Tyukh <roost1982@bigmir.net>
2023-01-11 14:08:52 +0200

    return  btn Load more

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,12 @@
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 import PropTypes from 'prop-types';
-import { ButtonStyled } from './Button.styled';
+
 
 const ButtonLoadMore = ({ onIncrement }) => {
     return (
-      //   <LoadMoreBtnStyled type="button" onClick={onClick} hidden="hidden">
-      //     Load more
-      //   </LoadMoreBtnStyled>
-
-      <ButtonStyled type="button" onClick={onIncrement} hidden="hidden">
-     Load more 
-      </ButtonStyled>
+      <Button onClick={onIncrement} variant="contained" size="large">
+        Load More
+      </Button>
     );
 };
 
